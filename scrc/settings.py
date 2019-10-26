391ea632c9f0f76b79ac0fd5c59da75b6d3d522b
iammujtaba <mujtaba.aamir1@gmail.com>
2019-10-26 20:30:43 +0530

    Confused What's going on

--- scrc/settings.py
+++ scrc/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
-
+import django_heroku
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -118,8 +118,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
+
 # MANAGING THE MEDIA
 PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 MEDIA_ROOT= os.path.join(BASE_DIR,"media")
 MEDIA_URL='/media/'
+django_heroku.settings(locals())
